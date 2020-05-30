b03d3a623750da4cd5790902a9507541524f2b6f
GabiiCata <gabiicata94@gmail.com>
2020-05-30 14:18:21 -0300

    guard config

--- front-end/donatusuerte/src/app/guards/auth.guard.ts
+++ front-end/donatusuerte/src/app/guards/auth.guard.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate,  Router  } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+  constructor ( private router : Router){}
+
+  canActivate()
+  {
+    if ( localStorage.getItem ('id') != null && localStorage.getItem ('id') != "" )
+    {
+      return true
+    }else{
+      this.router.navigate ( ['/sign-in']  );
+      return false;
+    }
   }
-  
 }
