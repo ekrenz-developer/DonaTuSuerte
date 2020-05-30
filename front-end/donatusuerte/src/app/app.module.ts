05c0443cebbaeaafccc06869ddaa139b1c692f9d
newagefacu <swfacu@gmail.com>
2020-05-30 12:20:30 -0300

    login Components

--- front-end/donatusuerte/src/app/app.module.ts
+++ front-end/donatusuerte/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './login/sign-in/sign-in.component';
+import { SignUpComponent } from './login/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
