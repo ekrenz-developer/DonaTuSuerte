8b68e3dd9220ec71265264bc1ec782d5c90bae67
newagefacu <swfacu@gmail.com>
2020-05-30 21:10:03 -0300

    DashboardComp

--- front-end/donatusuerte/src/app/app.module.ts
+++ front-end/donatusuerte/src/app/app.module.ts
@@ -7,11 +7,17 @@ import { SignInComponent } from './login/sign-in/sign-in.component';
 import { SignUpComponent } from './login/sign-up/sign-up.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardOrgComponent } from './dashboard-org/dashboard-org.component';
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    NavbarComponent,
+    SidebarComponent,
+    DashboardOrgComponent
   ],
   imports: [
     BrowserModule,
