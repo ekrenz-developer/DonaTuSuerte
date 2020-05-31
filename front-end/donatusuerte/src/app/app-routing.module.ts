f06924b3a75a9b29b620915217c11ad8be92bb36
GabiiCata <gabiicata94@gmail.com>
2020-05-30 21:36:03 -0300

    auth guard implement

--- front-end/donatusuerte/src/app/app-routing.module.ts
+++ front-end/donatusuerte/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignInComponent } from './login/sign-in/sign-in.component';
 import { SignUpComponent } from './login/sign-up/sign-up.component';
 import { DashboardOrgComponent } from './dashboard-org/dashboard-org.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
 const routes: Routes = 
 [
   { path: '', component: SignInComponent },
-  { path: 'dashboard-organization', component: DashboardOrgComponent },
+  { path: 'dashboard-organization', component: DashboardOrgComponent, canActivate:[AuthGuard] },
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
 ];
