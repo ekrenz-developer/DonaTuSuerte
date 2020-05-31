c478758e3898eff4f44cf6694c0e2e2723f09aad
GabiiCata <gabiicata94@gmail.com>
2020-05-30 21:24:23 -0300

    sidebar implementation - close sesion

--- front-end/donatusuerte/src/app/sidebar/sidebar.component.ts
+++ front-end/donatusuerte/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestService } from 'src/app/services/request.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  user:any
 
-  ngOnInit(): void {
+  constructor( private request : RequestService , private router : Router ) {
+    this.user = JSON.parse( localStorage.getItem ( 'user' ) ); 
+   }
+
+  ngOnInit() {
+  }
+
+  closeSesion(){
+    
+    Swal.fire({
+      title: '¿Esta seguro?',
+      text: "Esta por cerrar su sesion actual",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText : 'Cancelar', 
+      confirmButtonText: 'Cerrar Sesion'
+    }).then((result) => {
+      if (result.value) {
+        localStorage.clear()
+        Swal.fire(
+          'Sesion finalizada',
+          'Que tenga buen día',
+          'success'
+        )
+        this.router.navigate ( ['/sign-in']  );
+      }
+    })
+    
   }
 
 }
