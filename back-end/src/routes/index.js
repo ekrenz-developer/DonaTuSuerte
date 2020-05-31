0c7f90e4fc963343305740a424755292ec5e9f32
Erik Krenz <ekrenz.developer@gmail.com>
2020-05-31 09:13:45 -0300

    draw module

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,12 +2,14 @@ import userRouter from './userRouter';
 import roleRouter from './roleRouter';
 import organizationRouter from './organizationRouter';
 import storeRouter from './storeRouter';
+import drawRouter from './drawRouter';
 
 const setRoutes = server => {
   userRouter(server);
   roleRouter(server);
   organizationRouter(server);
   storeRouter(server);
+  drawRouter(server);
 
   server.get('/', (req, res) => {
     res.status(200).send({
